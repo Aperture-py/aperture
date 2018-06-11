d81855dbe8719cfa68654820a6213eaac5ff56af
jamessalvatore <jsalvatore171@gmail.com>
2018-06-11 17:36:56 -0400

    Added mock to util tests.

--- tests/util/files_test.py
+++ tests/util/files_test.py
@@ -52,57 +52,60 @@ class FilesTest(UtilTest):
             2. Get files with depth > 0.
             3. Get files from directory with no read access.
         '''
-
+        # Create fake file entries (mimics an instance of a os.DirEntry class returned from os.scandir)
         f1 = fakeFileEntry('file-1')
         f2 = fakeFileEntry('file-2')
         f3 = fakeFileEntry('file-3')
         f4 = fakeFileEntry('file-4')
         f5 = fakeFileEntry('file-5')
 
+        total_files = 5
+
+        # Test 1
         t_1_files = [f1, f2, f3]
-        mock_scandir.side_effect = [t_1_files]
+        mock_scandir.side_effect = [t_1_files]  # Mock root dir
 
         files = get_files_in_directory_recursive(self.file_dir, 0)
-        print('files: ', files)
+        # Make sure os.scandir is called with correct path
         mock_scandir.assert_called_with(self.file_dir)
         self.assertEqual(
             len(files), len(t_1_files),
             'finds all the files in dir when depth is 0')
 
-        d1 = fakeFileEntry('dir-1', True)
-        d2 = fakeFileEntry('dir-2', True)
+        # End Test 1
 
-        t_2_files_lvl_1 = [f1, f2, d1]
-        t_2_files_lvl_2 = [f3, f4, d2]
-        t_2_files_lvl_3 = [f5]
+        # Create fake directories
+        d1 = fakeFileEntry('dir-1', True)  # Mock dir at root
+        d2 = fakeFileEntry('dir-2', True)  # Mock dir nested below root
+        # Mock the a nested dir structure:
+        #
+        # file
+        # file
+        # dir
+        #   | file
+        #   | file
+        #   | dir
+        #     | file
 
-        t_2_total_files = 4
+        # Test 2
+        t_2_files_lvl_1 = [f1, f2, d1]  # Depth = 0
+        t_2_files_lvl_2 = [f3, f4, d2]  # Depth = 1
+        t_2_files_lvl_3 = [f5]  # Depth = 2
 
+        # Mock the return values for each depth
         mock_scandir.side_effect = [
             t_2_files_lvl_1, t_2_files_lvl_2, t_2_files_lvl_3
         ]
 
         files = get_files_in_directory_recursive(None, 2)
         self.assertEqual(
-            len(files), 4, 'finds all files in root and 2 nested dirs')
+            len(files), total_files,
+            'finds all files in root and 2 nested dirs')
 
-        # make temp dir
-        # make temp files
-        # test 1
-
-        # files = get_files_in_directory_recursive(self.file_dir, 0)
-
-        # self.assertEqual(
-        #     len(files), self.num_files_root,
-        #     'gets only files in root dir when depth is 0')
-
-        # files = get_files_in_directory_recursive(self.file_dir, 1)
-
-        # total_files = self.num_files_root + self.num_files_nested
-
-        # self.assertEqual(
-        #     len(files), total_files,
-        #     'gets files in root and sub directory when depth is 1')
+        # Test 3
+        mock_scandir.side_effect = PermissionError()
+        self.assertRaises(PermissionError, get_files_in_directory_recursive,
+                          self.file_dir)
 
     # TODO: Needs tests
     @unittest.skip('need to implement bytes_to_readable test')
