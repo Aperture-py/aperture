37d005415e63671a9b7e1354fc59bb418a42505e
Darrien Kennedy <darrien.kennedy@yahoo.com>
2018-06-17 01:41:28 -0400

    Color extension fix.

--- aperture/util/output.py
+++ aperture/util/output.py
@@ -1,21 +1,14 @@
 import os, sys, logging
 import aperture.util.files as utl_f
-''' 
-prev colors
-    'INFO': '\033[0m',
-    'SUCC': '\033[92m',
-    'WARN': '\033[93m',
-    'ERROR': '\033[91m',
-'''
 
 COLORS = {
     'INFO': '\u001b[0m',
     'SUCC': '\u001b[32m',
     'WARN': '\u001b[33m',
     'ERROR': '\u001b[31m',
-    '.jpg': '\u001b[36;1m',
-    '.gif': '\u001b[34;1m',
-    '.png': '\u001b[35;1m'
+    '.jpg': '\u001b[38;5;81m',
+    '.gif': '\u001b[38;5;229m',
+    '.png': '\u001b[38;5;198m'
 }
 
 widths = (40, 40, 10)
@@ -34,7 +27,7 @@ def log(message, level='INFO'):
     output = ''
 
     # If the os is windows ignore coloring
-    if not 'nt' == os.name:
+    if not os.name == 'nt':
         output += COLORS[level]
 
     print(output + message)
@@ -42,7 +35,7 @@ def log(message, level='INFO'):
     # Set the color to white after
     # NOTE: if there is some way to detect the default text
     # color this would be nice to apply here instead
-    if not 'nt' == os.name:
+    if not os.name == 'nt':
         sys.stdout.write(COLORS['INFO'])
 
 
@@ -65,7 +58,7 @@ def display_verbose_table(files):
     for image_index in range(image_count):
         orig_line = True
         orig = files['orig'][image_index]
-        line = '\t{} | '.format(get_table_filename(orig).ljust(widths[0]))
+        line = '\t{} | '.format(get_table_filename(orig, True).ljust(widths[0]))
 
         for output_index in range(1, len(files)):
             current = files[list(files)[output_index]][image_index]
@@ -93,28 +86,23 @@ def get_table_filename(file_tuple, color_ext=False):
     filename = os.path.split(file_tuple[0])[1]
     filesize = '[{}]'.format(utl_f.bytes_to_readable(file_tuple[1]))
 
-    # determine size of spaces between the two..
-
     filename_space = widths[0] - len(filesize)
 
+    fn, ext = os.path.splitext(filename)
     if len(filename) < filename_space:
         space_count = filename_space - len(filename)
-        trunc_name = filename
+        trunc_name = fn
     else:
         # Compress filename with an elipses
         space_count = 1
-        # 17 + ext
-        fn, ext = os.path.splitext(filename)
-        len(fn)
+        trunc_name = '{}...{}'.format(
+            fn[:filename_space - len(ext) - len(fn) - 21], fn[len(fn) - 17:])
 
-        if color_ext == True and not os.name == 'nt':
-            temp_ext = ext.lower()
-            if temp_ext == '.jpeg':
-                temp_ext = '.jpg'
-            ext = '{}{}{}'.format(COLORS[temp_ext], ext, COLORS['INFO'])
+    if color_ext and not os.name == 'nt':
+        temp_ext = ext.lower()
+        if temp_ext == '.jpeg':
+            temp_ext = '.jpg'
+        new_ext = '{}{}{}'.format(COLORS[temp_ext], ext, COLORS['INFO'])
+        ext = new_ext
 
-        trunc_name = '{}...{}{}'.format(
-            fn[:filename_space - len(ext) - len(fn) - 21], fn[len(fn) - 17:],
-            ext)
-
-    return '{}{}{}'.format(trunc_name, ' ' * space_count, filesize)
+    return '{}{}{}{}'.format(trunc_name, ext, ' ' * space_count, filesize)
